c4ff50af577e6bdce5ccfd5eaccc5be63ce08047
Rahad Ullah <myemail.abc20@gmail.com>
2025-05-04 09:45:13 +0600

    Defined all sidebar items

--- src/utils/sidebarItems.tsx
+++ src/utils/sidebarItems.tsx
@@ -1,18 +1,25 @@
 import { BiCategory } from 'react-icons/bi';
 import { TSidebarItem } from './generateSidebarItems';
-
-import { AiOutlineFileText, AiOutlineQuestionCircle, AiOutlineStar } from 'react-icons/ai';
-import { CiMedal } from 'react-icons/ci';
-import { IoBarChartOutline } from 'react-icons/io5';
-import { BsMegaphone, BsPersonGear } from 'react-icons/bs';
-import { TbLogout, TbUserDollar } from 'react-icons/tb';
-import { CalendarRange, Contact, Users } from 'lucide-react';
+import { TbUserDollar } from 'react-icons/tb';
+import {
+    CalendarRange,
+    ChartPie,
+    CircleDollarSignIcon,
+    Contact,
+    Gem,
+    Gift,
+    LogOut,
+    Settings,
+    ShieldUser,
+    Users,
+    WalletMinimal,
+} from 'lucide-react';
 const sidebarItems: TSidebarItem[] = [
     {
         key: 'analytics',
         label: 'Analytics',
         path: '',
-        icon: <IoBarChartOutline size={24} />,
+        icon: <ChartPie size={24} />,
     },
     {
         key: 'users',
@@ -34,10 +41,10 @@ const sidebarItems: TSidebarItem[] = [
         ],
     },
     {
-        key: 'brands',
-        label: 'Brands',
-        path: 'brands',
-        icon: <CiMedal size={24} />,
+        key: 'subscription-plan',
+        label: 'Subscription Plan',
+        path: 'subscription-plan',
+        icon: <WalletMinimal size={24} />,
     },
     {
         key: 'subscriber',
@@ -58,40 +65,40 @@ const sidebarItems: TSidebarItem[] = [
         icon: <CalendarRange size={24} />,
     },
     {
-        key: 'reviews',
-        label: 'Reviews',
-        path: 'reviews',
-        icon: <AiOutlineStar size={24} />,
+        key: 'payments',
+        label: 'Payments',
+        path: 'payments',
+        icon: <CircleDollarSignIcon size={24} />,
     },
     {
-        key: 'campaign',
-        label: 'Campaign',
-        path: 'campaign',
-        icon: <BsMegaphone size={24} />,
+        key: 'settings',
+        label: 'Settings',
+        path: 'settings',
+        icon: <Settings size={24} />,
     },
     {
-        key: 'add-admin',
-        label: 'Add Admin',
-        path: 'make-admin',
-        icon: <BsPersonGear size={24} />,
+        key: 'manage-admin',
+        label: 'Manage Admin',
+        path: 'manage-admin',
+        icon: <ShieldUser size={24} />,
     },
     {
-        key: 'terms',
-        label: 'Terms & Conditions',
-        path: 'terms',
-        icon: <AiOutlineFileText size={24} />,
+        key: 'bonus',
+        label: 'Bonus & Challenges',
+        path: 'bonus',
+        icon: <Gift size={24} />,
     },
     {
-        key: 'faqs',
-        label: 'FAQs',
-        path: 'faqs',
-        icon: <AiOutlineQuestionCircle size={24} />,
+        key: 'referral',
+        label: 'Referral',
+        path: 'referral',
+        icon: <Gem size={24} />,
     },
     {
         key: 'logout',
         label: 'Log Out',
         path: 'login',
-        icon: <TbLogout size={24} />,
+        icon: <LogOut size={24} />,
     },
 ];
 
