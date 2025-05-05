e34e01c07c2879fd12aaa0684501d8f5552c9151
Rahad Ullah <myemail.abc20@gmail.com>
2025-05-05 17:15:24 +0600

    Added OTP verify page

--- src/pages/authentication/VerifyOtp.tsx
+++ src/pages/authentication/VerifyOtp.tsx
@@ -1,85 +1,92 @@
 import { Button, ConfigProvider, Form, FormProps, Input } from 'antd';
 import { FieldNamesType } from 'antd/es/cascader';
 import { useNavigate } from 'react-router-dom';
+import login from '../../assets/login.png';
 
-const VerifyOtp = () => {
+const ForgetPassword = () => {
     const navigate = useNavigate();
     const onFinish: FormProps<FieldNamesType>['onFinish'] = (values) => {
         console.log('Received values of form: ', values);
-        navigate('/new-password');
+        navigate('/verify-otp');
     };
 
     return (
         <ConfigProvider
             theme={{
+                token: {
+                    colorPrimary: '#9558b7',
+
+                    // colorBgContainer: '#F1F4F9',
+                },
                 components: {
                     Input: {
-                        // lineHeight: 3,
-                        controlHeight: 50,
-
-                        borderRadius: 10,
+                        borderRadius: 40,
+                        colorBorder: 'transparent',
+                        colorPrimaryBorder: 'transparent',
+                        hoverBorderColor: 'transparent',
+                        controlOutline: 'none',
+                        activeBorderColor: 'transparent',
                     },
-                },
-                token: {
-                    colorPrimary: '#DAA520',
                 },
             }}
         >
-            <div className="flex bg-[#1A4F73] items-center justify-center h-screen">
-                <div className="bg-white w-[630px] rounded-lg shadow-lg p-10 ">
-                    <div className="text-primaryText space-y-3 text-center">
-                        <h1 className="text-3xl  font-medium text-center mt-2">Check your email</h1>
-                        <p>
-                            We sent a reset link to contact@dscode...com enter 5 digit code that mentioned in the email
-                        </p>
-                    </div>
+            <div className="grid grid-cols-2  items-center">
+                <div>
+                    <img src={login} alt="" className="w-full object-cover h-screen" />
+                </div>
 
-                    <Form
-                        name="normal_VerifyOtp"
-                        className="my-5"
-                        layout="vertical"
-                        initialValues={{ remember: true }}
-                        onFinish={onFinish}
-                    >
-                        <Form.Item
-                            className="flex items-center justify-center mx-auto"
-                            name="otp"
-                            rules={[{ required: true, message: 'Please input otp code here!' }]}
-                        >
-                            <Input.OTP
-                                style={{
-                                    width: 300,
-                                }}
-                                className=""
-                                variant="filled"
-                                length={5}
-                            />
-                        </Form.Item>
-
-                        <Form.Item>
-                            <Button
-                                shape="round"
-                                type="primary"
-                                htmlType="submit"
-                                style={{
-                                    height: 45,
-                                    width: '100%',
-                                    fontWeight: 500,
-                                }}
-                                // onClick={() => navigate('/')}
-                            >
-                                Verify OTP Code
-                            </Button>
-                        </Form.Item>
-                        <div className="text-center text-lg flex items-center justify-center gap-2">
-                            <p className="text-primaryText">Didn't receive the code?</p>
-                            <p className="text-primary">Resend code</p>
+                <div className=" flex items-center justify-center bg-[#F1F1F1] min-h-screen">
+                    <div className="max-w-sm">
+                        <div className=" space-y-3 text-center">
+                            <h1 className="text-2xl text-[#111111] font-semibold text-center mt-2">
+                                Verification code
+                            </h1>
+                            <p className="text-primaryText">
+                                We sent a reset link to contact@dscode...com enter 5 digit code that is mentioned in the
+                                email
+                            </p>
                         </div>
-                    </Form>
+
+                        <Form
+                            name="normal_VerifyOtp"
+                            className="my-5"
+                            layout="vertical"
+                            initialValues={{ remember: true }}
+                            onFinish={onFinish}
+                        >
+                            <Form.Item
+                                className="flex items-center justify-center mx-auto"
+                                name="otp"
+                                rules={[{ required: true, message: 'Please input otp code here!' }]}
+                            >
+                                <Input.OTP length={5} size='large' />
+                            </Form.Item>
+
+                            <Form.Item>
+                                <Button
+                                    shape="round"
+                                    type="primary"
+                                    htmlType="submit"
+                                    style={{
+                                        height: 45,
+                                        width: '100%',
+                                        fontWeight: 500,
+                                    }}
+                                    // onClick={() => navigate('/')}
+                                >
+                                    Verify OTP Code
+                                </Button>
+                            </Form.Item>
+                            <div className="text-center flex items-center justify-center gap-2">
+                                <p className="text-primaryText font-medium">Didn't receive the code?</p>
+                                <p className="text-primary font-medium">Resend code</p>
+                            </div>
+                        </Form>
+                    </div>
                 </div>
             </div>
         </ConfigProvider>
     );
 };
 
-export default VerifyOtp;
+export default ForgetPassword;
