2265529d8cb282c7e86fc95ce5d7bd27b17e7c25
Rahad Ullah <myemail.abc20@gmail.com>
2025-05-21 09:49:32 +0600

    Added redux toolkit and RTK query setup

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,8 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const baseApi = createApi({
+    reducerPath: 'baseApi',
+    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_BASE_URL }),
+    endpoints: () => ({}),
+    tagTypes: ['Users'],
+});
