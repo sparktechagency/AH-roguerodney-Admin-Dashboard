81c8a7678a8dda68fbf49b3c9446fbb3666d8425
Rahad Ullah <myemail.abc20@gmail.com>
2025-05-24 12:42:05 +0600

    Implemented artist plan

--- src/pages/dashboard/subscriptionPlans/ClientSubscriptions.tsx
+++ src/pages/dashboard/subscriptionPlans/ClientSubscriptions.tsx
@@ -1,237 +1,33 @@
-import { Button, Form, Input } from 'antd';
-import { CircleCheck, CircleMinus, PencilLineIcon, Plus } from 'lucide-react';
+import { PencilLineIcon } from 'lucide-react';
 import { useState } from 'react';
-import CustomModal from '../../../components/shared/CustomModal';
 import { useGetAllPlansQuery } from '../../../redux/features/plan/planApi';
+import EditPlanForm from './forms/EditPlanForm';
 
 const ClientSubscriptions = () => {
-    const [addPlanModal, setAddPlanModal] = useState(false);
     const [editPlanModal, setEditPlanModal] = useState(false);
-    const [editModalData, setEditModalData] = useState(null);
-    const [packageOffers, setPackageOffers] = useState([
-        ' Top priority in client matching (get booked first)',
-        'No service fees – Keep 100% of your earnings',
-        'Exclusive Ah-Luxe Challenges (earn extra even without bookings)',
-        'Stock up and Glow rewards and bonus cash prize opportunities',
-        'Profile featured on the Ooh Ah homepage',
-    ]);
+    const [editModalData, setEditModalData] = useState<
+        { name?: string; price?: number; price_offer?: number; offers?: string[] } | undefined
+    >(undefined);
 
     const { data } = useGetAllPlansQuery({ query: `for=USER` });
     const package1 = data?.data[0];
     const package2 = data?.data[1];
     const package3 = data?.data[2];
 
-    const addPlanForm = (
-        <Form layout="vertical">
-            <Form.Item label={<label className="font-medium">Package Name</label>} name="name">
-                <Input
-                    style={{
-                        height: 42,
-                    }}
-                    placeholder="Enter package name"
-                    className="text-base font-medium rounded-md"
-                />
-            </Form.Item>
-
-            <Form.Item label={<label className="font-medium">Package Price</label>} name="price">
-                <Input
-                    style={{
-                        height: 42,
-                    }}
-                    placeholder="$19.99/month"
-                    className="text-base font-medium rounded-md"
-                />
-            </Form.Item>
-
-            <Form.Item label={<label className="font-medium">Package Price offer</label>} name="price_offer">
-                <Input
-                    type="number"
-                    style={{
-                        height: 42,
-                    }}
-                    placeholder="Enter package price offer"
-                    className="text-base font-medium rounded-md"
-                />
-            </Form.Item>
-
-            <Form.Item label={<label className="font-medium flex">Package Offers</label>} name="offers">
-                <div className="flex items-center gap-2">
-                    <Input
-                        style={{
-                            height: 42,
-                        }}
-                        placeholder="Enter package offers"
-                        className="text-base font-medium rounded-md"
-                    />
-                    <Button
-                        type="default"
-                        style={{ height: 42 }}
-                        className="rounded-md"
-                        onClick={() => {
-                            const newOffer = (
-                                document.querySelector('input[placeholder="Enter package offers"]') as HTMLInputElement
-                            )?.value;
-                            if (newOffer) {
-                                setPackageOffers([...packageOffers, newOffer]);
-                            }
-                        }}
-                    >
-                        <Plus />
-                    </Button>
-                </div>
-
-                <ul className="grid gap-2 p-4 border rounded-lg mt-4">
-                    {packageOffers?.map((item, idx) => (
-                        <li key={idx} className="flex items-center justify-between gap-4">
-                            <p className="flex gap-2">
-                                <CircleCheck className="size-5 min-w-5 text-green-600" />
-                                {item}
-                            </p>
-                            <button
-                                onClick={() => {
-                                    const updatedOffers = packageOffers.filter((_, index) => index !== idx);
-                                    setPackageOffers(updatedOffers);
-                                }}
-                            >
-                                <CircleMinus className="text-stone-500" />
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-            </Form.Item>
-
-            <Form.Item>
-                <div className="flex justify-center w-full">
-                    <Button
-                        htmlType="submit"
-                        type="primary"
-                        style={{
-                            height: 40,
-                        }}
-                        className="w-full text-base rounded-lg"
-                    >
-                        Submit
-                    </Button>
-                </div>
-            </Form.Item>
-        </Form>
-    );
-
-    const editPlanForm = (
-        <Form layout="vertical">
-            <Form.Item label={<label className="font-medium">Package Name</label>} name="name">
-                <Input
-                    style={{
-                        height: 42,
-                    }}
-                    placeholder="Enter package name"
-                    className="text-base font-medium rounded-md"
-                />
-            </Form.Item>
-
-            <Form.Item label={<label className="font-medium">Package Price</label>} name="price">
-                <Input
-                    style={{
-                        height: 42,
-                    }}
-                    placeholder="$19.99/month"
-                    className="text-base font-medium rounded-md"
-                />
-            </Form.Item>
-
-            <Form.Item label={<label className="font-medium">Package Price offer</label>} name="price_offer">
-                <Input
-                    type="number"
-                    style={{
-                        height: 42,
-                    }}
-                    placeholder="Enter package price offer"
-                    className="text-base font-medium rounded-md"
-                />
-            </Form.Item>
-
-            <Form.Item label={<label className="font-medium flex">Package Offers</label>} name="offers">
-                <div className="flex items-center gap-2">
-                    <Input
-                        style={{
-                            height: 42,
-                        }}
-                        placeholder="Enter package offers"
-                        className="text-base font-medium rounded-md"
-                    />
-                    <Button
-                        type="default"
-                        style={{ height: 42 }}
-                        className="rounded-md"
-                        onClick={() => {
-                            const newOffer = (
-                                document.querySelector('input[placeholder="Enter package offers"]') as HTMLInputElement
-                            )?.value;
-                            if (newOffer) {
-                                setPackageOffers([...packageOffers, newOffer]);
-                            }
-                        }}
-                    >
-                        <Plus />
-                    </Button>
-                </div>
-
-                <ul className="grid gap-2 p-4 border rounded-lg mt-4">
-                    {packageOffers?.map((item, idx) => (
-                        <li key={idx} className="flex items-center justify-between gap-4">
-                            <p className="flex gap-2">
-                                <CircleCheck className="size-5 min-w-5 text-green-600" />
-                                {item}
-                            </p>
-                            <button
-                                onClick={() => {
-                                    const updatedOffers = packageOffers.filter((_, index) => index !== idx);
-                                    setPackageOffers(updatedOffers);
-                                }}
-                            >
-                                <CircleMinus className="text-stone-500" />
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-            </Form.Item>
-
-            <Form.Item>
-                <div className="flex justify-center w-full">
-                    <Button
-                        htmlType="submit"
-                        type="primary"
-                        style={{
-                            height: 40,
-                        }}
-                        className="w-full text-base rounded-lg"
-                    >
-                        Submit
-                    </Button>
-                </div>
-            </Form.Item>
-        </Form>
-    );
-
     return (
         <section className="">
-            <div className="flex justify-end">
-                <Button
-                    onClick={() => setAddPlanModal(true)}
-                    type="primary"
-                    style={{ height: 42 }}
-                    className="rounded-xl text-base"
-                >
-                    Add Package
-                </Button>
-            </div>
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 pt-4">
                 {/* Ah Casual  */}
                 {package1 && (
                     <div className="flex flex-col">
                         <div className="flex items-center justify-between gap-2 p-5 pb-9 bg-teal-400 text-white rounded-2xl">
                             <h3 className="text-2xl font-medium">{package1?.name}</h3>
-                            <button onClick={() => setEditPlanModal(true)}>
+                            <button
+                                onClick={() => {
+                                    setEditPlanModal(true);
+                                    setEditModalData(package1);
+                                }}
+                            >
                                 <PencilLineIcon />
                             </button>
                         </div>
@@ -255,7 +51,12 @@ const ClientSubscriptions = () => {
                     <div className="flex flex-col">
                         <div className="flex items-center justify-between gap-2 p-5 pb-9 bg-[#6A3E82] text-white rounded-2xl">
                             <h3 className="text-2xl font-medium">{package2?.name}</h3>
-                            <button onClick={() => setEditPlanModal(true)}>
+                            <button
+                                onClick={() => {
+                                    setEditPlanModal(true);
+                                    setEditModalData(package2);
+                                }}
+                            >
                                 <PencilLineIcon />
                             </button>
                         </div>
@@ -279,7 +80,12 @@ const ClientSubscriptions = () => {
                     <div className="flex flex-col">
                         <div className="flex items-center justify-between gap-2 p-5 pb-9 bg-[#2190F2] text-white rounded-2xl">
                             <h3 className="text-2xl font-medium">{package3?.name}</h3>
-                            <button onClick={() => setEditPlanModal(true)}>
+                            <button
+                                onClick={() => {
+                                    setEditPlanModal(true);
+                                    setEditModalData(package3);
+                                }}
+                            >
                                 <PencilLineIcon />
                             </button>
                         </div>
@@ -304,19 +110,11 @@ const ClientSubscriptions = () => {
                     </div>
                 )}
 
-                <CustomModal
-                    open={addPlanModal}
-                    setOpen={setAddPlanModal}
-                    title="Add Package"
-                    width={500}
-                    body={addPlanForm}
-                />
-                <CustomModal
+                <EditPlanForm
+                    defaultData={editModalData}
                     open={editPlanModal}
                     setOpen={setEditPlanModal}
-                    title="Edit Package"
-                    width={500}
-                    body={editPlanForm}
+                    setEditModalData={setEditModalData}
                 />
             </div>
         </section>
---
+++ src/pages/dashboard/subscriptionPlans/forms/EditPlanForm.tsx
@@ -0,0 +1,168 @@
+import { Button, Form, Input, Modal } from 'antd';
+import { CircleCheck, CircleMinus, Plus } from 'lucide-react';
+import React, { useEffect } from 'react';
+import toast from 'react-hot-toast';
+import { useUpdatePlanMutation } from '../../../../redux/features/plan/planApi';
+
+const EditPlanForm = ({
+    defaultData,
+    open,
+    setOpen,
+    setEditModalData,
+}: {
+    defaultData: any;
+    open: boolean;
+    setOpen: (open: boolean) => void;
+    setEditModalData: (data: any) => void;
+}) => {
+    const [packageOffers, setPackageOffers] = React.useState<string[]>(defaultData?.offers || []);
+    const [form] = Form.useForm();
+    const [updatePlan] = useUpdatePlanMutation();
+
+    useEffect(() => {
+        if (defaultData) {
+            form.setFieldsValue({
+                name: defaultData.name,
+                price: defaultData.price,
+                price_offer: defaultData.price_offer,
+                offers: packageOffers,
+            });
+            setPackageOffers(defaultData.offers || []);
+        }
+    }, [defaultData]);
+
+    // handle form submission
+    const handleSubmit = async (values: any) => {
+        toast.loading('Updating package...', { id: 'update-package' });
+        try {
+            const res = await updatePlan({
+                id: defaultData._id,
+                payload: {
+                    ...values,
+                    price: Number(values.price),
+                    price_offer: Number(values.price_offer),
+                    offers: packageOffers,
+                    price_id: defaultData.price_id,
+                    productId: defaultData.productId,
+                },
+            }).unwrap();
+            if (res.success) {
+                toast.success('Package updated successfully', { id: 'update-package' });
+                setOpen(false);
+                setEditModalData(null);
+            }
+        } catch (error) {
+            toast.error('Failed to update package', { id: 'update-package' });
+            console.error(error);
+        }
+    };
+
+    return (
+        <Modal
+            title="Edit Package"
+            width={500}
+            open={open}
+            onCancel={() => {
+                setOpen(false);
+                setEditModalData(null);
+            }}
+            footer={false}
+        >
+            <Form layout="vertical" onFinish={handleSubmit} form={form}>
+                <Form.Item label={<label className="font-medium">Package Name</label>} name="name">
+                    <Input
+                        style={{
+                            height: 42,
+                        }}
+                        placeholder="Enter package name"
+                        className="text-base font-medium rounded-md"
+                    />
+                </Form.Item>
+
+                <Form.Item label={<label className="font-medium">Package Price</label>} name="price">
+                    <Input
+                        style={{
+                            height: 42,
+                        }}
+                        placeholder="Enter package price"
+                        className="text-base font-medium rounded-md"
+                    />
+                </Form.Item>
+
+                <Form.Item label={<label className="font-medium">Package Price offer</label>} name="price_offer">
+                    <Input
+                        style={{
+                            height: 42,
+                        }}
+                        placeholder="Enter package price offer"
+                        className="text-base font-medium rounded-md"
+                    />
+                </Form.Item>
+
+                <Form.Item label={<label className="font-medium flex">Package Offers</label>} name="offers">
+                    <div className="flex items-center gap-2">
+                        <Input
+                            style={{
+                                height: 42,
+                            }}
+                            placeholder="Enter package offers"
+                            className="text-base font-medium rounded-md"
+                        />
+                        <Button
+                            type="default"
+                            style={{ height: 42 }}
+                            className="rounded-md"
+                            onClick={() => {
+                                const newOffer = (
+                                    document.querySelector(
+                                        'input[placeholder="Enter package offers"]',
+                                    ) as HTMLInputElement
+                                )?.value;
+                                if (newOffer) {
+                                    setPackageOffers([...packageOffers, newOffer]);
+                                }
+                            }}
+                        >
+                            <Plus />
+                        </Button>
+                    </div>
+                </Form.Item>
+                <ul className="grid gap-2 p-4 border rounded-lg mb-4">
+                    {packageOffers?.map((item, idx) => (
+                        <li key={idx} className="flex items-center justify-between gap-4">
+                            <p className="flex gap-2">
+                                <CircleCheck className="size-5 min-w-5 text-green-600" />
+                                {item}
+                            </p>
+                            <button
+                                onClick={() => {
+                                    const updatedOffers = packageOffers.filter((_, index) => index !== idx);
+                                    setPackageOffers(updatedOffers);
+                                }}
+                            >
+                                <CircleMinus className="text-stone-500" />
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+
+                <Form.Item>
+                    <div className="flex justify-center w-full">
+                        <Button
+                            htmlType="submit"
+                            type="primary"
+                            style={{
+                                height: 40,
+                            }}
+                            className="w-full text-base rounded-lg"
+                        >
+                            Submit
+                        </Button>
+                    </div>
+                </Form.Item>
+            </Form>
+        </Modal>
+    );
+};
+
+export default EditPlanForm;
