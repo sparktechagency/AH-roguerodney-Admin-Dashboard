363303e790d1e1370d9b25d838a7bcc7835353eb
Rahad Ullah <myemail.abc20@gmail.com>
2025-05-25 09:14:02 +0600

    Implemented service api

---
+++ src/redux/features/service/serviceApi.ts
@@ -0,0 +1,61 @@
+import { baseApi } from '../../api/baseApi';
+
+const subCategoryApi = baseApi.injectEndpoints({
+    endpoints: (builder) => ({
+        getAllService: builder.query({
+            query: ({ query }) => {
+                return {
+                    url: `/serviceManagement${query}`,
+                    method: 'GET',
+                };
+            },
+            providesTags: ['Services', 'Service'],
+        }),
+        getSingleService: builder.query({
+            query: ({ id }) => {
+                return {
+                    url: `/serviceManagement/${id}`,
+                    method: 'GET',
+                };
+            },
+            providesTags: ['Service'],
+        }),
+        createService: builder.mutation({
+            query: ({ payload }) => {
+                return {
+                    url: `/serviceManagement/create`,
+                    method: 'POST',
+                    body: payload,
+                };
+            },
+            invalidatesTags: ['Services', 'Service'],
+        }),
+        updateService: builder.mutation({
+            query: ({ id, payload }) => {
+                return {
+                    url: `/serviceManagement/${id}`,
+                    method: 'PATCH',
+                    body: payload,
+                };
+            },
+            invalidatesTags: ['Services', 'Service'],
+        }),
+        deleteService: builder.mutation({
+            query: ({ id }) => {
+                return {
+                    url: `/serviceManagement/${id}`,
+                    method: 'DELETE',
+                };
+            },
+            invalidatesTags: ['Services', 'Service'],
+        }),
+    }),
+});
+
+export const {
+    useCreateServiceMutation,
+    useUpdateServiceMutation,
+    useDeleteServiceMutation,
+    useGetAllServiceQuery,
+    useGetSingleServiceQuery,
+} = subCategoryApi;
