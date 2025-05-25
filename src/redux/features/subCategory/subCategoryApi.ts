8b6c1b39050868a146ec25aa141282da9a298098
Rahad Ullah <myemail.abc20@gmail.com>
2025-05-25 08:50:54 +0600

    Defined sub category api

---
+++ src/redux/features/subCategory/subCategoryApi.ts
@@ -0,0 +1,51 @@
+import { baseApi } from '../../api/baseApi';
+
+const subCategoryApi = baseApi.injectEndpoints({
+    endpoints: (builder) => ({
+        getAllSubCategory: builder.query({
+            query: ({ query }) => {
+                return {
+                    url: `/subCategory${query}`,
+                    method: 'GET',
+                };
+            },
+            providesTags: ['SubCategories', 'SubCategory'],
+        }),
+        getSingleSubCategory: builder.query({
+            query: ({ id }) => {
+                return {
+                    url: `/subCategory/${id}`,
+                    method: 'GET',
+                };
+            },
+            providesTags: ['SubCategory'],
+        }),
+        createSubCategory: builder.mutation({
+            query: ({ payload }) => {
+                return {
+                    url: `/subCategory/create`,
+                    method: 'POST',
+                    body: payload,
+                };
+            },
+            invalidatesTags: ['SubCategories', 'SubCategory'],
+        }),
+        updateSubCategory: builder.mutation({
+            query: ({ id, payload }) => {
+                return {
+                    url: `/subCategory/${id}`,
+                    method: 'PATCH',
+                    body: payload,
+                };
+            },
+            invalidatesTags: ['SubCategories', 'SubCategory'],
+        }),
+    }),
+});
+
+export const {
+    useCreateSubCategoryMutation,
+    useUpdateSubCategoryMutation,
+    useGetAllSubCategoryQuery,
+    useGetSingleSubCategoryQuery,
+} = subCategoryApi;
