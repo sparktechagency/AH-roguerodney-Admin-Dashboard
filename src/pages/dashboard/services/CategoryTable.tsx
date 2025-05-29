c4ea418db6ac014839b230db8df03f92e325e684
Rahad Ullah <myemail.abc20@gmail.com>
2025-05-29 10:46:21 +0600

    Optimized category table and edit category

--- src/pages/dashboard/services/CategoryTable.tsx
+++ src/pages/dashboard/services/CategoryTable.tsx
@@ -1,36 +1,24 @@
-import { Button, ConfigProvider, Form, Input, Table, UploadFile } from 'antd';
+import { Button, ConfigProvider, Table } from 'antd';
 import { Plus } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { IoTrashOutline } from 'react-icons/io5';
-import UploadImage from '../../../components/shared/UploadImage';
-import { useEditCategoryMutation, useGetAllCategoriesQuery } from '../../../redux/features/category/categoryApi';
+import { useGetAllCategoriesQuery } from '../../../redux/features/category/categoryApi';
 import { IMAGE_URL } from '../../../redux/api/baseApi';
-import toast from 'react-hot-toast';
-import EditCategoryModal from './EditCategoryModal';
 import MyModal from '../../../components/shared/MyModal';
 import AddCategoryForm from './forms/category/AddCategoryForm';
 import DeleteCategoryForm from './forms/category/DeleteCategoryForm';
+import EditCategoryForm from './forms/category/EditCategoryForm';
 
 const CategoryTable = () => {
     const [addCategoryModal, setAddCategoryModal] = useState(false);
     const [editCategoryModal, setEditCategoryModal] = useState(false);
-    const [editCategoryData, setEditCategoryData] = useState();
     const [deleteCategoryModal, setDeleteCategoryModal] = useState(false);
+    const [activeCaterory, setActiveCategory] = useState();
     const [currentCategoryId, setCurrentCategoryId] = useState(null);
-
-    const [fileList, setFileList] = useState<UploadFile[]>([]);
-    const [editCategory] = useEditCategoryMutation();
-    const [editForm] = Form.useForm();
 
     const { data } = useGetAllCategoriesQuery(undefined);
     const categoryData = data?.data;
-
-    useEffect(() => {
-        if (editCategoryData) {
-            editForm.setFieldsValue(editCategoryData);
-        }
-    }, [editCategoryData]);
 
     const columns = [
         {
@@ -62,21 +50,15 @@ const CategoryTable = () => {
                     <button
                         onClick={() => {
                             setEditCategoryModal(true);
-                            setEditCategoryData(item);
-                            setCurrentCategoryId(item._id);
-                            setFileList(
-                                item?.image?.map((img: string, idx: number) => ({
-                                    uid: img + idx,
-                                    name: img,
-                                    status: 'done',
-                                    url: `${IMAGE_URL}${img}`,
-                                    thumbUrl: `${IMAGE_URL}${img}`,
-                                })),
-                            );
+                            setActiveCategory(item);
                         }}
                     >
                         <AiOutlineEdit className="text-xl text-primary" />
                     </button>
+                    {/* edit modal */}
+                    <MyModal open={editCategoryModal} setOpen={setEditCategoryModal} key={item._id}>
+                        <EditCategoryForm itemData={activeCaterory} setEditCategoryModal={setEditCategoryModal} />
+                    </MyModal>
                     <button
                         onClick={() => {
                             setDeleteCategoryModal(true);
@@ -85,79 +67,18 @@ const CategoryTable = () => {
                     >
                         <IoTrashOutline className="text-xl text-red-500" />
                     </button>
+                    {/* delete modal */}
+                    <MyModal open={deleteCategoryModal} setOpen={setDeleteCategoryModal}>
+                        <DeleteCategoryForm
+                            open={deleteCategoryModal}
+                            setOpen={setDeleteCategoryModal}
+                            itemId={currentCategoryId ?? ''}
+                        />
+                    </MyModal>
                 </div>
             ),
         },
     ];
-
-    // handle edit category form
-    const handleEditCategory = async (values: any) => {
-        toast.loading('Editing category...', { id: 'edit-category' });
-        const formData = new FormData();
-        formData.append('name', values.name);
-        if (fileList && fileList.length > 0) {
-            fileList.forEach((file) => {
-                if (file.originFileObj) {
-                    formData.append('image', file.originFileObj);
-                } else {
-                    formData.append('existImage', file?.name);
-                }
-            });
-        }
-
-        try {
-            const res = await editCategory({
-                id: currentCategoryId,
-                payload: formData,
-            }).unwrap();
-            if (res?.success) {
-                toast.success('Category updated successfully', { id: 'edit-category' });
-                setEditCategoryModal(false);
-            }
-        } catch (error) {
-            console.error('Error editing category:', error);
-            toast.error('Failed to edit category', { id: 'edit-category' });
-        }
-    };
-
-    const editCategoryForm = (
-        <Form
-            style={{
-                color: '#767676',
-            }}
-            layout="vertical"
-            form={editForm}
-            onFinish={handleEditCategory}
-            // initialValues={editCategoryData}
-        >
-            <Form.Item label="Category Name" name="name">
-                <Input
-                    style={{
-                        height: 42,
-                    }}
-                    placeholder="Enter category name"
-                />
-            </Form.Item>
-
-            <Form.Item label="Category Images" name="images">
-                <UploadImage fileList={fileList} setFileList={setFileList} maxCount={5} />
-            </Form.Item>
-
-            <Form.Item>
-                <div className="flex justify-center w-full">
-                    <Button
-                        htmlType="submit"
-                        type="primary"
-                        style={{
-                            height: 40,
-                        }}
-                    >
-                        Edit category
-                    </Button>
-                </div>
-            </Form.Item>
-        </Form>
-    );
 
     return (
         <div className="grid gap-4 mt-2">
@@ -173,23 +94,6 @@ const CategoryTable = () => {
             <ConfigProvider>
                 <Table columns={columns} dataSource={categoryData} />
             </ConfigProvider>
-
-            {/* delete modal */}
-            <MyModal open={deleteCategoryModal} setOpen={setDeleteCategoryModal}>
-                <DeleteCategoryForm
-                    open={deleteCategoryModal}
-                    setOpen={setDeleteCategoryModal}
-                    itemId={currentCategoryId ?? ''}
-                />
-            </MyModal>
-
-            {/* edit modal */}
-            <EditCategoryModal
-                open={editCategoryModal}
-                setOpen={setEditCategoryModal}
-                children={editCategoryForm}
-                setEditCategoryData={setEditCategoryData}
-            />
         </div>
     );
 };
---
+++ src/pages/dashboard/services/forms/category/EditCategoryForm.tsx
@@ -0,0 +1,100 @@
+import { Button, Form, Input, UploadFile } from 'antd';
+import UploadImage from '../../../../../components/shared/UploadImage';
+import toast from 'react-hot-toast';
+import { useEffect, useState } from 'react';
+import { useEditCategoryMutation } from '../../../../../redux/features/category/categoryApi';
+import { IMAGE_URL } from '../../../../../redux/api/baseApi';
+
+const EditCategoryForm = ({ setEditCategoryModal, itemData }: { setEditCategoryModal: any; itemData: any }) => {
+    const [fileList, setFileList] = useState<UploadFile[]>([]);
+    const [editCategory] = useEditCategoryMutation();
+    const [editForm] = Form.useForm();
+
+    useEffect(() => {
+        if (itemData) {
+            editForm.setFieldsValue(itemData);
+            setFileList(
+                itemData?.image?.map((img: string, idx: number) => ({
+                    uid: img + idx,
+                    name: img,
+                    status: 'done',
+                    url: `${IMAGE_URL}${img}`,
+                    thumbUrl: `${IMAGE_URL}${img}`,
+                })),
+            );
+        }
+    }, [itemData]);
+
+    // handle edit category form
+    const handleEditCategory = async (values: any) => {
+        toast.loading('Editing category...', { id: 'edit-category' });
+        const formData = new FormData();
+        formData.append('name', values.name);
+        if (fileList && fileList.length > 0) {
+            fileList.forEach((file) => {
+                if (file.originFileObj) {
+                    formData.append('image', file.originFileObj);
+                } else {
+                    formData.append('existImage', file?.name);
+                }
+            });
+        }
+
+        try {
+            const res = await editCategory({
+                id: itemData?._id,
+                payload: formData,
+            }).unwrap();
+            if (res?.success) {
+                toast.success('Category updated successfully', { id: 'edit-category' });
+                setEditCategoryModal(false);
+            }
+        } catch (error) {
+            console.error('Error editing category:', error);
+            toast.error('Failed to edit category', { id: 'edit-category' });
+        }
+    };
+
+    return (
+        <div>
+            <Form
+                style={{
+                    color: '#767676',
+                }}
+                layout="vertical"
+                form={editForm}
+                onFinish={handleEditCategory}
+                initialValues={itemData}
+            >
+                <Form.Item label="Category Name" name="name">
+                    <Input
+                        style={{
+                            height: 42,
+                        }}
+                        placeholder="Enter category name"
+                    />
+                </Form.Item>
+
+                <Form.Item label="Category Images" name="images">
+                    <UploadImage fileList={fileList} setFileList={setFileList} maxCount={5} />
+                </Form.Item>
+
+                <Form.Item>
+                    <div className="flex justify-center w-full">
+                        <Button
+                            htmlType="submit"
+                            type="primary"
+                            style={{
+                                height: 40,
+                            }}
+                        >
+                            Edit category
+                        </Button>
+                    </div>
+                </Form.Item>
+            </Form>
+        </div>
+    );
+};
+
+export default EditCategoryForm;
